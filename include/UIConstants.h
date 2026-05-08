e8b4215a3ee06225bf5253e293d99f78de6bc54e
Hexaredecimal <mfanakagama@gmail.com>
2026-05-09 01:22:24 +0200

    rasen: Retry again

--- include/UIConstants.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace UIConstants {
-    const float PADDING_X = 20.0f;
-    const float PADDING_Y = 10.0f;
-    const int   FONT_SIZE = 12;
-    const float CHECKBOX_RADIUS = 10;
-}
-
