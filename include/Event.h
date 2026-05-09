873a3538d79d3d42fe82ae8e06de212e1faa8213
Hexaredecimal <mfanakagama@gmail.com>
2026-05-09 08:58:37 +0200

    Event.h: Added an event for the checkbox

--- include/Event.h
+++ include/Event.h
@@ -7,16 +7,21 @@
 template <typename TypeWidget>
 class Event {
 public:
-    Event(Vector2& position, TypeWidget* widget):
+    Event(Vector2& position, TypeWidget* widget, bool checked = false):
         m_position(position),
-        m_widget(widget) {}
+        m_widget(widget),
+        m_checked(checked) {}
+
     ~Event() {}
 
     Vector2 mouse_position() { return m_position; };
     TypeWidget* trigger_widget() { return m_widget; };
+    bool is_checked() { return m_checked; }
+
 private:
     Vector2 m_position;
     TypeWidget* m_widget;
+    bool m_checked;
 };
 
 template <typename W>
@@ -24,6 +29,9 @@ using ClickEvent = std::function<void(Event<W>&)>;
 
 template <typename W>
 using HoverEvent = std::function<void(Event<W>&)>;
+
+template <typename W>
+using CheckEvent = std::function<void(Event<W>&)>;
 
 template <typename TypeWidget>
 class EventWidget : public Widget {
@@ -34,9 +42,13 @@ public:
     void on_hover(HoverEvent<TypeWidget> e) { m_on_hover = e; }
     HoverEvent<TypeWidget> on_hover() { return m_on_hover; }
 
+    void on_check(CheckEvent<TypeWidget> e) { m_on_check = e; }
+    CheckEvent<TypeWidget> on_check() { return m_on_check; }
+
 private:
     ClickEvent<TypeWidget> m_on_click = nullptr;
     HoverEvent<TypeWidget> m_on_hover = nullptr;
+    CheckEvent<TypeWidget> m_on_check = nullptr;
 };
 
 
